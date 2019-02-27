b05133657f0af7689de1700545f0c7d766f891e9
Yue Pi <42617039+SherryPi@users.noreply.github.com>
2019-02-27 15:58:07 -0500

    Update get method in user table

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 var r = require("rethinkdb");
 var obj2 = {host: '34.204.183.169', port: 28015, user: 'admin'};
 var connection = null;
-r.connect( obj2, function(err, conn) {//connect to the database
+
+//connect to our database
+r.connect( obj2, function(err, conn) {
     if (err){
 	console.log("cannot connect");
         console.log(err);
@@ -15,16 +17,33 @@ r.connect( obj2, function(err, conn) {//connect to the database
 
 });
 
-//the function below is just for test, will be replaced later by more specific api calls and functions
-//once the code is running on server, the address http://3.93.183.130:3000/ will return:
-//'[ { "email": "piyue@live.unc.edu", "number": 9195993474, "password": "lalala", "user_id": 1, "username": "piyue" } ]'
+var weatherwayz = r.db("Weatherwayz");
+
+//initial page for the api
 app.get('/', (req, res) => {
-    r.db("Weatherwayz").table('Users').
+    res.send("Welcome to WeatherWayz API!\n");
+})
+
+//api calls for table "Users"
+
+//get the ALL users and their information in database
+app.get('/users',(req, res) => {
+    weatherwayz.table('Users').
     run(connection, function(err, result) {
-        if (err) throw err;
+        if (err) res.send(err);
         res.send(JSON.stringify(result._responses[0].r, null, 2));
+    });
+
+})
+
+//get information of a specific user with its user_id
+//if the user_id does not exist, it returns null
+app.get('/users/:user_id',(req, res) => {
+    weatherwayz.table('Users').get(parseInt(req.params.user_id)).
+    run(connection, function(err, result) {
+        if (err) res.send(err);
+        res.send(JSON.stringify(result, null, 2));
     });
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
-
